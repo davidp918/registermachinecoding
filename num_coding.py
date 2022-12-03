31003bafe1a9bcfca94c9cc0319e56d9366cdede
Exusiai <david.yijie.pan@outlook.com>
2022-12-03 14:22:20 +0000

    Fix dec_pair when code is odd, x would be 0

--- num_coding.py
+++ num_coding.py
@@ -1,113 +1,128 @@
 def enc_pair(x, y):
-# Encode <<x, y>>
-	return (2 ** x * (2 * y + 1))
+    # Encode <<x, y>>
+    return (2 ** x * (2 * y + 1))
+
 
 def dec_pair(code):
-# Decode <<x, y>>
-	x = 0
-	y = 0
+    # Decode <<x, y>>
+    x = 0
+    y = 0
 
-	if code % 2 == 1:
-		x = 1
-	else:
-		while code % 2 == 0:
-			code = code // 2
-			x += 1
-	y = (code - 1) / 2
+    while code % 2 == 0:
+        code = code // 2
+        x += 1
+    y = (code - 1) / 2
 
-	return(x, y)
+    return(x, y)
+
 
 def enc_pair_alt(x, y):
-# Encode <x, y>
-	return (2 ** x * (2 * y + 1) - 1)
+    # Encode <x, y>
+    return (2 ** x * (2 * y + 1) - 1)
+
 
 def dec_pair_alt(code):
-# Decode <x, y>
-	return dec_pair(code + 1)
+    # Decode <x, y>
+    return dec_pair(code + 1)
+
 
 def enc_list(l):
-	if l == []:
-		return 0
-	else:
-		return enc_pair( l[0], enc_list(l[1:]) )
+    if l == []:
+        return 0
+    else:
+        return enc_pair(l[0], enc_list(l[1:]))
+
 
 def dec_list(code):
-	res = []
+    res = []
 
-	while code != 0:
-		(item, tail) = dec_pair(code)
-		code = tail
-		res.append(item)
+    while code != 0:
+        (item, tail) = dec_pair(code)
+        code = tail
+        res.append(item)
 
-	return res
+    return res
+
 
 def dec_instr(code):
-	res = []
+    res = []
 
-	if code == 0:
-		return res
+    if code == 0:
+        return res
 
-	(i, j) = dec_pair(code)
+    (i, j) = dec_pair(code)
 
-	if i % 2 == 0:
-		res.append(i // 2)
-		res.append(j)
+    if i % 2 == 0:
+        res.append(i // 2)
+        res.append(j)
 
-	else:
-		res.append((i - 1) // 2)
-		(k, l) = dec_pair_alt(j)
-		res.append(k)
-		res.append(l)
+    else:
+        res.append((i - 1) // 2)
+        (k, l) = dec_pair_alt(j)
+        res.append(k)
+        res.append(l)
 
-	return res
+    return res
+
 
 def enc_instr(instr):
-# Representations:
-# HALT = []
-# Ri + -> Rj = [i, j]
-# Ri - -> Rj, Rk = [i, j, k]
-	if len(instr) == 0:
-		return 0
-	elif len(instr) == 2:
-		return enc_pair(2 * instr[0], instr[1])
+    # Representations:
+    # HALT = []
+    # Ri + -> Rj = [i, j]
+    # Ri - -> Rj, Rk = [i, j, k]
+    if len(instr) == 0:
+        return 0
+    elif len(instr) == 2:
+        return enc_pair(2 * instr[0], instr[1])
 
-	goto = enc_pair_alt(instr[1], instr[2])
-	return enc_pair(2 * instr[0] + 1, goto)
+    goto = enc_pair_alt(instr[1], instr[2])
+    return enc_pair(2 * instr[0] + 1, goto)
+
 
 def enc_rm(machine):
-# Encode register machine instruction by instruction
-	res = []
-	for instr in machine:
-		res.append(enc_instr(instr))
+    # Encode register machine instruction by instruction
+    res = []
+    for instr in machine:
+        res.append(enc_instr(instr))
 
-	return enc_list(res)
+    return enc_list(res)
+
 
 def dec_rm(code):
-	res = []
-	instrs = dec_list(code)
+    res = []
+    instrs = dec_list(code)
 
-	for instr in instrs:
-		res.append(dec_instr(instr))
+    for instr in instrs:
+        res.append(dec_instr(instr))
 
-	return res
+    return res
+
 
 def print_rm(rm):
-	for instr in rm:
-		if len(instr) == 0:
-			print("HALT")
-		elif len(instr) == 2:
-			print("R" + str(instr[0]) + " + -> R" + str(instr[1]))
-		else:
-			print("R" + str(instr[0]) + " - -> R" + str(instr[1]) \
-			+ ", " + str(instr[2]))
+    for instr in rm:
+        if len(instr) == 0:
+            print("HALT")
+        elif len(instr) == 2:
+            print("R" + str(instr[0]) + " + -> R" + str(instr[1]))
+        else:
+            print("R" + str(instr[0]) + " - -> R" + str(instr[1])
+                  + ", " + str(instr[2]))
 
-print(enc_pair(3, 0))
-print(enc_pair(1, 8))
-print(dec_pair(276))
-print("")
-print(enc_list([2,1,3]))
-print(dec_list(276))
-print(dec_pair_alt(27))
-print("")
-print(enc_instr([1,1,2]))
-print(dec_instr(152))
+
+p1 = (0, 0)
+p2 = (1, 2)
+
+r1 = enc_pair(*p1)
+r2 = enc_pair_alt(*p2)
+
+print(r1)
+print(r2)
+
+d1 = dec_list(2 ** 94 * 16395)
+print(d1)
+
+print(dec_pair(8197))
+
+print(enc_list([94, 0, 1, 10]))
+
+print(dec_instr(1))
